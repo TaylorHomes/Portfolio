d2c2e04458026efa551ec4d417cea40beaf70e5f
TaylorHomes <taylor.homes01@gmail.com>
2019-05-16 11:03:17 -0400

    work

--- notebook/wsgi.py
+++ notebook/wsgi.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise import WhiteNoise
+
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notebook.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root='/static/')
+application = DjangoWhiteNoise(application)
