4766eb7dde7045fef83e27349028e407fb9f3adb
TaylorHomes <taylor.homes01@gmail.com>
2019-05-16 12:12:54 -0400

    debug

--- notebook/settings.py
+++ notebook/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&n%p%hh(kmyo#@--*s-(xpd+e!2p+lm+ddk#m2-rr_6-v!k^^w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
